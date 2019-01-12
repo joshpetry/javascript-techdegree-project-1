aaca90a5f87078e8a68def114068539aa742430e
Josh Petry <josh.petry@outlook.com>
2019-01-12 13:19:41 -0500

    Completed Project
    Updated comments and getRandomQuote()

--- js/script.js
+++ js/script.js
@@ -3,19 +3,7 @@ Treehouse Techdegree:
 FSJS project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-
-  Recommended: 
-    - Add at least one `year` and/or `citation` property to at least one 
-      quote object.   
-***/
-
+//Array of quote objects
 var quotes = [
     {
         //Scenerio #1: citation and year exist - works
@@ -50,66 +38,35 @@ var quotes = [
     }
 ];
 
-
-/***
-  Create the `getRandomQuote` function to:
-   - generate a random number 
-   - use the random number to `return` a random quote object from the 
-     `quotes` array.
-***/
-
+//Function to generate a random quote object
 function getRandomQuote() {
-    return Math.floor((Math.random() * 5));
+    return quotes[Math.floor((Math.random() * 5))];
 }
 
-/***
-  Create the `printQuote` function to: 
-   - call the `getRandomQuote` function and assign it to a variable.
-   - use the properties of the quote object stored in the variable to 
-     create your HTML string.
-   - use conditionals to make sure the optional properties exist before 
-     they are added to the HTML string.
-   - set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
+//Function to generate quote string and randomly display different quotes
 function printQuote() {
     var randomQuote = getRandomQuote();
-    var printedQuote = "<p class=\"quote\">" + quotes[randomQuote].quote + "</p>";
-    printedQuote += "<p class=\"source\">" + quotes[randomQuote].source;
+    var printedQuote = "<p class=\"quote\">" + randomQuote.quote + "</p>";
+    printedQuote += "<p class=\"source\">" + randomQuote.source;
     
     //Scenerio #1: citation and year exist
-    if (quotes[randomQuote].citation !== undefined && quotes[randomQuote].year !== undefined) {
-        printedQuote += "<span class=\"citation\">" + quotes[randomQuote].citation + "</span>" 
-        printedQuote += "<span class=\"year\">" + quotes[randomQuote].year + "</span></p>" 
-    
+    if (randomQuote.citation !== undefined && randomQuote.year !== undefined) {
+        printedQuote += "<span class=\"citation\">" + randomQuote.citation + "</span>" 
+        printedQuote += "<span class=\"year\">" + randomQuote.year + "</span></p>" 
     //Scenerio #2: only citation exists
-    } else if (quotes[randomQuote].citation !== undefined)  {
-    printedQuote += "<span class=\"citation\">" + quotes[randomQuote].citation+ "</span></p>" 
-    
+    } else if (randomQuote.citation !== undefined)  {
+    printedQuote += "<span class=\"citation\">" + randomQuote.citation+ "</span></p>" 
     //Scenerio #3: only year exists
-    } else if (quotes[randomQuote].year !== undefined)  {
-        printedQuote += "<span class=\"year\">" + quotes[randomQuote].year + "</span></p>" 
-        
+    } else if (randomQuote.year !== undefined)  {
+        printedQuote += "<span class=\"year\">" + randomQuote.year + "</span></p>" 
     //Scenerio #4: neither citation nor year exist   
     } else {
         printedQuote += "</p>";
     }
-    return printedQuote;
+    
+    //Replace HTML in #quote-box with printedQuote string
+    document.getElementById("quote-box").innerHTML = printedQuote;
 }
 
-/*
-      <p class="quote">Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.</p>
-      <p class="source">Patrick McKenzie<span class="citation">Twitter</span><span class="year">2016</span></p>
-*/
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
-
+//Call printQuote function on button clikc
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
